4f91f4451e416586ec97edfdb3a959e9067b0218
Kevin <kevin005005@gmail.com>
2021-05-01 19:15:19 +0200

    First project moved to uploader since that's the project that will upload the data

--- Uploader/Uploader/Program.cs
+++ Uploader/Uploader/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Net;
+using System.Xml.Linq;
+using System.Linq;
+using System.Collections.Generic;
 
-namespace Uploader
+namespace Downloader
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            List<Article> articles = ArticleService.DownloadArticlesFromTelex();
+            List<string> jsonArticles = ArticleService.ArticlesToJson(articles);
         }
     }
 }

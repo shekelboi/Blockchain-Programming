3e7394fc93c4a9ea5f4b270a640a3fc729ad54f8
Kevin <kevin005005@gmail.com>
2021-05-07 17:43:27 +0200

    Useful messages added.

--- Uploader/Uploader/Uploader.cs
+++ Uploader/Uploader/Uploader.cs
@@ -11,31 +11,32 @@ namespace Blockchain_Programming.Uploader
 {
     class Uploader
     {
-        static void Main(string[] args)
+        static void Main()
         {
             List<Article> articles = ArticleService.DownloadArticlesFromTelex();
             List<string> jsonArticles = ArticleService.ArticlesToJson(articles);
 
             Token token = ModexService.ObtainToken().Result;
             File.WriteAllText("token.dat", JsonSerializer.Serialize(token));
+            Console.WriteLine("Token obtained!");
 
-            List<EntityResponse> entityResponses = UploadData(jsonArticles, "thisisatest", token);
+            List<EntityResponse> entityResponses = UploadData(jsonArticles, "thisisatest34", token);
             File.WriteAllText("entities.dat", JsonSerializer.Serialize(entityResponses));
         }
 
         static List<EntityResponse> UploadData(List<string> jsonArticles, string schemaName, Token token)
         {
             EntityResponse entityResponse = ModexService.CreateEntity(File.ReadAllText("schema.json"), schemaName, token.access_token).Result;
+            Console.WriteLine("Schema created!");
 
-            List<EntityResponse> entityResponses = new List<EntityResponse>();
+            List <EntityResponse> entityResponses = new List<EntityResponse>();
 
             foreach (var article in jsonArticles)
             {
                 entityResponse = ModexService.UploadRecord(article, schemaName, token.access_token).Result;
                 entityResponses.Add(entityResponse);
+                Console.WriteLine("Record added!");
             }
-
-            Schema record = ModexService.ViewRecord(schemaName, entityResponse.recordId, token.access_token).Result;
 
             return entityResponses;
         }

5ee8aa7f8664fa902f34fcfe8482f841d61c058f
Kevin <kevin005005@gmail.com>
2021-05-08 18:16:24 +0200

    Convesion to XML added.

--- Downloader/Downloader.cs
+++ Downloader/Downloader.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Text.Json;
+using System.Xml.Serialization;
 
 namespace Blockchain_Programming.Downloader
 {
@@ -14,15 +15,23 @@ namespace Blockchain_Programming.Downloader
             Token token = JsonSerializer.Deserialize<Token>(File.ReadAllText("token.dat"));
             EntityContainer entityContainer = JsonSerializer.Deserialize<EntityContainer>(File.ReadAllText("entities.dat"));
 
+            List<Article> articles = new List<Article>();
+
             foreach (var item in entityContainer.EntityResponses)
             {
                 Schema schema = ModexService.ViewRecord(entityContainer.SchemaName, item.recordId, token).Result;
+                articles.Add(schema.Article);
 
                 Console.WriteLine(schema.Article.Title);
                 Console.WriteLine(schema.Article.Summary);
                 Console.WriteLine(schema.Article.PublicationDate);
                 Console.WriteLine();
             }
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Article>));
+            StreamWriter streamWriter = new StreamWriter("out.xml");
+            xmlSerializer.Serialize(streamWriter, articles);
+            streamWriter.Close();
 
             Console.WriteLine("Process finished");
             Console.ReadKey();
